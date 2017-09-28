0b2c18124f54400619bb2980683553f8d5afa1e1
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2017-09-28 19:39:47 -0400

    first real commit

--- server.rb
+++ server.rb
@@ -5,6 +5,18 @@ require "sinatra/activerecord"
 set :views, Proc.new { File.join(root, "app/views") }
 
 get "/" do
-  erb :index
+  erb :sign_in
+end
+
+post "/sign_in" do
+
+end
+
+get "/new_user" do
+  erb :new_user
+end
+
+post "/user/new" do
+
 end
 
