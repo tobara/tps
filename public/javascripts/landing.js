40c444b6a25dcc7727d5ce13a6a77676f23374ab
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2017-10-07 16:04:31 -0400

    needs some work

---
+++ public/javascripts/landing.js
@@ -0,0 +1,9 @@
+
+function openForm(e) {
+    e.preventDefault();
+    document.getElementById("loginForm").style.height = "100%";
+}
+
+function closeForm() {
+    document.getElementById("loginForm").style.height = "0%";
+}
