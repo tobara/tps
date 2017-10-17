8e07e759742c6cfff8de92801f625b9df9044904
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2017-10-17 10:53:54 -0400

    let kevlarr get to the bottom of this

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,12 @@
+require 'pry'
 class UserMailer < ActionMailer::Base
   default :from => "application_name@domain.com"
 
   def registration_confirmation(user)
-    mail(:to => "#{user.first_name} <#{user.email}>", :subject => "Registration Confirmation for Awesome App", :body => "http://localhost:9393/#{user.confirm_token}/confirm_email/")
+    # mail(:to => "#{user.first_name} <#{user.email}>", :subject => "Registration Confirmation for Awesome App", :body => "http://localhost:9393/#{user.confirm_token}/confirm_email/")
+  mail(:to => "#{user.first_name} <#{user.email}>", :subject => "Registration Confirmation for Awesome App") do |format|
+      format.text
+      format.html
+    end
   end
 end
