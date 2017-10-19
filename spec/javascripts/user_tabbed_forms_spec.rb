f1f8fde0c60d33674397af186ff798e9cca44dab
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2017-10-19 11:00:25 -0400

    test arrangement

--- spec/javascripts/user_tabbed_forms_spec.rb
+++ spec/javascripts/user_tabbed_forms_spec.rb
@@ -5,8 +5,10 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'pry'
 
-Capybara.javascript_driver = :poltergeist
 Capybara.app = Sinatra::Application
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+end
 set(:show_exceptions, false)
 
 
